ff5dbfc1bbd8c6d6ca7f907813db79c131b45a80
ecmchugh <ethan.c.mchugh.28@dartmouth.edu>
2025-12-14 17:57:22 -0600

    first push

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+export default function Home() {
+  return (
+    <main>
+      <nav>
+        <span> Daily</span>
+      </nav>
+      <header>
+        <h1>Daily Journal</h1>
+        <p>15 Minutes Each Day.</p>
+      </header>
+
+      <textarea placeholder="Start writing here..." />
+    </main>
+  );
+}
